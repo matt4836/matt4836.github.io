c5e4235490d4f5b83786b81bf67c3ff44ab1a31e
Matt <ms@Touchy.local>
2018-03-16 09:43:46 -0400

    update

---
+++ vendor/custom.js
@@ -0,0 +1,11 @@
+// window.onload = function(){
+// $("*").find(".glyphicon").toggleClass("rotated");
+// $("*").find(".collapse").toggleClass("show");
+// };
+
+
+
+
+$(".chevron-rotate").click(function(){
+  $(this).find(".glyphicon").toggleClass("rotated");
+});
